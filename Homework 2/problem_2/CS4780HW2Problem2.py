947513a9c1a173d4c3e213e5e1dde7d185d96948
Tech Kuo <thk42@cornell.edu>
2014-09-18 21:08:02 -0400

    Pushing Homework 2 Problem 2 stuff
    Just skeleton files.

---
+++ Homework 2/problem_2/CS4780HW2Problem2.py
@@ -0,0 +1,10 @@
+# Tech Kuo(thk42) and Ben Wang(bhw44)
+# CS 4780 Machine Learning
+# 9/18/2014
+# Homework 2 Problem 2
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
