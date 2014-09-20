eae013344e3568fe39946dfcc6c5d456105b59b7
Tech Kuo <thk42@cornell.edu>
2014-09-20 17:18:21 -0400

    Edited Node class
    Redefined constructor, defined calc_entropy and made it static, defined
    to_string, and defined is_pure.

--- Homework 2/problem_2/CS4780HW2Problem2.py
+++ Homework 2/problem_2/CS4780HW2Problem2.py
@@ -10,7 +10,7 @@ import sys
 
 class Node:
     
-	def __init__(self, attr, val, ben, mal, items):
+	def __init__(self, attr, val, ben, mal, attr_range, entropy, items):
 		"""
 		precondition: attr, val are the attribute value pair this node will split on.
 			ben and mal are the number of benign and malignint tumors respectively.
@@ -20,14 +20,23 @@ class Node:
 		self.attr = attr
 		self.val = val
 		self.items = items
-		self.pos = pos
-		self.neg = neg
-		self.entropy = self.calculate_entropy()
+		self.ben = ben
+		self.mal = mal
+		self.attr_range = attr_range
+		self.entropy = entropy 
 		self.left = None
 		self.right = None
 
-	def calculate_entropy():
-		TODO
-	
+	def __str__(self):
+		return "Attribute " + self.attr + "<= " + self.val + "\n" + "[" + 
+				self.ben + ", " + self.mal + "]"
+
+	def calculate_entropy(b,m):
+		p_plus = (b/b + m))
+		p_minus = (m/(b + m))
+		return -(p_plus * np.log2(p_plus) - p_minus * np.log2(p_minus))
+
+	def is_pure(self):
+		return (self.ben == 0) or (self.mal == 0)
 
 
