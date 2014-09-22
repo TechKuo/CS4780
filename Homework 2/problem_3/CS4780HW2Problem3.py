cb0916f2b8ec3f0c5ea3e80adc6596745a3b1985
Tech Kuo <thk42@cornell.edu>
2014-09-22 13:38:30 -0400

    Started code on problem 3aii
    Not sure if I'm collecting the right stats yet

--- Homework 2/problem_3/CS4780HW2Problem3.py
+++ Homework 2/problem_3/CS4780HW2Problem3.py
@@ -40,8 +40,29 @@ def get_confidence(stats):
         cis.append(ci)
     return cis
 
-pa = get_prediction_arrays(sys.argv[1])
+"""pa = get_prediction_arrays(sys.argv[1])
 stats = get_stats(pa)
 cis = get_confidence(stats)
 print "\nConfidence interval for kNN: " + str(cis[0]) + "\n"
-print "Confidence interval for Decision Tree: " + str(cis[1]) + "\n"
+print "Confidence interval for Decision Tree: " + str(cis[1]) + "\n" """
+
+def get_error_rates(f):
+    data_file = open(f)
+    total_num = 0 # holds total num of observations
+    error_rates = [0,0]
+    for line in data_file:
+        total_num += 1
+        contents = line.split(',')
+        for i in range(1,3):
+            if i == 1:
+                if contents[0] != contents[i]:
+                    error_rates[i-1]+=1 # holds num_errors for kNN
+            if i == 2:
+                if contents[0] != (contents[i].strip()):
+                    error_rates[i-1]+=1 # holds num_erros for decision tree
+    for i in range(2):
+        error_rates[i] = error_rates[i]/float(total_num)
+    return error_rates
+
+er = get_error_rates(sys.argv[1])
+print er
