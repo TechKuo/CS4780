e489300a01dca7046de7af415c3c646815fb6c4a
Ben Wang <benedicthwang@gmail.com>
2014-09-22 21:46:47 -0400

    yo yo, my name wang, i don't go slow, i code with a bang, swag

--- Homework 2/problem_2/CS4780HW2Problem2.py
+++ Homework 2/problem_2/CS4780HW2Problem2.py
@@ -4,23 +4,27 @@
 # Homework 2 Problem 2
 
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import copy
-
 
 class Node:
 
     num_nodes = 0
     num_leaves = 0
-    depth = 0
+    tree_depth = 0
+    max_depth = 0
+    num_nodes_per_level = [0]*20
+    num_leaves_per_level = [0]*20
 
-    def __init__(self, ben, mal, attr_range, items, entropy):
+    def __init__(self, ben, mal, attr_range, items, entropy, depth):
         """
-        precondition: attr, val are the attribute value pair this node will split on.
+        precondition:
             ben and mal are the number of benign and malignint tumors respectively.
+            attr_range is a list of tuples representing the lower,upper bound of each attribute.
             items is list of indices of examples to classify.
-
+            entropy is the entropy of the instances represented by items.
         """
         self.attr = -1 # will be assigned in find_best_attribute
         self.val = -1 # will be assigned in find_best_attribute
@@ -31,41 +35,62 @@ class Node:
         self.entropy = entropy
         self.left = None
         self.right = None
-        Node.num_nodes += 1
-        self.num_nodes = Node.num_nodes
-        if (Node.num_nodes == 0):
-            Node.depth = 1
-        else:
-            Node.depth = int(math.floor(np.log2(Node.num_nodes))) + 1
-        self.depth = Node.depth
+        self.depth = depth
+
         if ben > mal:
             self.tree_label = 'B'
         else:
             self.tree_label = 'M'
-        if self.is_pure():
+        
+        Node.tree_depth = max(Node.tree_depth, self.depth)
+        Node.num_nodes += 1
+        if self.is_leaf():
             Node.num_leaves += 1
+            Node.num_leaves_per_level[self.depth-1] += 1
+        Node.num_nodes_per_level[self.depth-1] += 1
 
     def __str__(self):
         s = "Attribute " + str(self.attr) + " <= " + str(self.val) + "\n" 
         s += "Entropy: " + str(self.entropy) + "\n"
         s += "[" + str(self.ben) + ", " + str(self.mal) + "] \n"
-        s += "Number of Nodes Currently: " + str(self.num_nodes) + "\n"
         s += "Depth of Node: " + str(self.depth) + "\n"
         s +=  "Tree Label: " + self.tree_label + "\n"
         return s
 
     @staticmethod
     def calc_entropy(b, m):
+        """
+        precondition: b and m are the count of different types in a collection.
+        return: the entropy of this collection.
+        """
+
         if b == 0 or m == 0:
             return 0
         p_plus = b/float(b + m)
         p_minus = m/float(b + m)
         return -(p_plus * np.log2(p_plus) + p_minus * np.log2(p_minus))
 
-    def is_pure(self):
-        return (self.ben == 0) or (self.mal == 0)
+    def is_leaf(self):
+        """
+        return: this Node is a leaf.
+        """
+        return (self.depth == Node.max_depth) or (self.ben == 0) or (self.mal == 0)
 
     def find_best_attribute(self):
+        """
+        summary: Iterates through all possible attribute, value pairs and finds the best split.
+            Records the best attribute, value to split on in the fields of this Node
+
+        return: The values associated with splitting. Will be a list l.
+            l[0] = # of benign examples in the left subset
+            l[1] = # of malignint examples in the left subset
+            l[2] = a list of indices indicating the examples in the left subset
+            l[3] = entropy of the left subset
+            l[4] = # of benign examples in the right subset
+            l[5] = # of malignint examples in the right subset
+            l[6] = a list of indices indicating the examples in the right subset
+            l[7] = entropy of the right subset
+        """
         ben_lte = 0 #-1 indicates a leaf node
         mal_lte = 0 #-1 indicates a leaf node
         ben_gt = 0 #-1 indicates a leaf node
@@ -117,6 +142,9 @@ class Node:
         return rv
 
     def calc_values(self, attr, val):
+        """
+        summary: Helper function for find_best_attribute.
+        """
         ben_lte = 0
         mal_lte = 0
         ben_gt = 0
@@ -124,7 +152,7 @@ class Node:
 
         curr_lte_items = []
         curr_gt_items = []
-
+        
         for item in self.items:
             if train_set[item][attr] <= val:
                 curr_lte_items.append(item)
@@ -146,20 +174,27 @@ class Node:
         return ben_lte, mal_lte, curr_lte_items, entropy_lte, ben_gt, mal_gt, curr_gt_items, entropy_gt, gain
     
     def make_babies(self):
-        if not self.is_pure():
+        """
+        summary: Builds the decision tree rooted at this Node. 
+            Uses the id3 algorithm to find best splitting criterion.
+        """
+        if not self.is_leaf():
             rv = self.find_best_attribute()
             lte_attr_range = copy.deepcopy(self.attr_range)
             lte_attr_range[self.attr][1] = self.val
             gt_attr_range = copy.deepcopy(self.attr_range)
             gt_attr_range[self.attr][0] = self.val + 1
-            self.left = Node(rv[0], rv[1], lte_attr_range, rv[2], rv[3])
-            self.right = Node(rv[4], rv[5], gt_attr_range, rv[6], rv[7])
-            print self
+            self.left = Node(rv[0], rv[1], lte_attr_range, rv[2], rv[3], self.depth + 1)
+            self.right = Node(rv[4], rv[5], gt_attr_range, rv[6], rv[7], self.depth + 1)
             self.left.make_babies()
             self.right.make_babies()
     
-    def classify(self, item, d=depth):
-        if self.is_pure() or self.depth == d:
+    def classify(self, item):
+        """
+        precondition: item is the instance to be classified. 
+        returns: 'B' if item is classified benign, 'M' if item is classified malignint.
+        """
+        if self.is_leaf():
             return self.tree_label
         if item[self.attr] <= self.val:
             return self.left.classify(item)
@@ -167,6 +202,10 @@ class Node:
             return self.right.classify(item)
 
 def init_tree(items):
+    """
+    precondition: items is a list of indices of instances.
+    return: Root node of decision tree built by training on items.
+    """
     root_ben = 0
     root_mal = 0
     for example in items:
@@ -174,18 +213,23 @@ def init_tree(items):
             root_ben += 1
         else:
             root_mal += 1
-    root_entropy = Node.calc_entropy(root_ben, root_mal)
+    root_entr = Node.calc_entropy(root_ben, root_mal)
     root_items = range(len(items))
     root_attr_range = [[1,10] for x in range(10)]
-    root = Node(root_ben, root_mal, root_attr_range, root_items, root_entropy)
+    root_depth = 1
+    root = Node(root_ben, root_mal, root_attr_range, root_items, root_entr, root_depth)
     print root 
     root.make_babies()
     return root 
 
-def classify_items(dt,items,d=Node.depth):
+def classify_items(dt, items):
+    """
+    precondition: dt is a decision tree, items is a list of instances.
+    summary: Modifies each instance by appending the label chosen by dt.
+    """
     for example in items:
-        tree_label = dt.classify(example,d)
-        example.append(tree_label)
+        tree_label = dt.classify(example)
+        example[10] = tree_label
 
 def find_accuracy(classified_items):
     correct = 0
@@ -203,25 +247,58 @@ def parse_file(f):
         for i in range(1,10):
             temp = contents[i].split(':')
             contents[i] = int(temp[1])
-        l.append(contents[:10])
+        item = contents[:10]
+        item.append('N')
+        l.append(item)
+        # last location will be overwritten when instance is classified
+
     return l
 
 def print_root_and_children(dt):
-    print "Root:\n" + str(dt) + "\n"
-    print "Left Child:\n" + str(dt.left) + "\n"
-    print "Right Child:\n" + str(dt.right) + "\n"
-
+    print "Root:\n" + str(dt)
+    print "Left Child:\n" + str(dt.left)
+    print "Right Child:\n" + str(dt.right)
 
 train_set = parse_file(sys.argv[1])
-test_set = parse_file(sys.argv[2])
+val_set = parse_file(sys.argv[2])
+test_set = parse_file(sys.argv[3])
 decision_tree = init_tree(train_set)
-classify_items(decision_tree,train_set)
-classify_items(decision_tree,test_set)
+
+classify_items(decision_tree, train_set)
+classify_items(decision_tree, test_set)
 acc_train = find_accuracy(train_set)
 acc_test = find_accuracy(test_set)
 print "-----------Decision Tree----------- \n"
 print_root_and_children(decision_tree)
-print "Total Number of Nodes: " + str(Node.num_nodes) + "\n"
-print "Total Number of Leaves: " + str(Node.num_leaves) + "\n"
-print "Accuracy of training set: " + str(acc_train) + "\n"
-print "Accuracy of test set: " + str(acc_test) + "\n"
+print "Total Number of Nodes: " + str(Node.num_nodes)
+print "Total Number of Leaves: " + str(Node.num_leaves)
+print "Accuracy of training set: " + str(acc_train)
+print "Accuracy of test set: " + str(acc_test)
+print "Depth of tree is: " + str(Node.tree_depth)
+
+
+max_depths = range(2,21)
+val_acc = []
+test_acc = []
+
+print ''
+print 'Depth | Leaves | Nodes'
+print '----------------------'
+for depth in max_depths:
+    Node.max_depth = depth
+    leaves = sum(Node.num_leaves_per_level[:depth-1]) + Node.num_nodes_per_level[depth-1]
+    nodes = sum(Node.num_nodes_per_level[:depth])
+    print '  {0:2d}  |   {1:2d}   |  {2:2d}  '.format(depth, leaves, nodes)
+    classify_items(decision_tree,val_set)
+    classify_items(decision_tree, test_set)
+    val_acc.append(find_accuracy(val_set))
+    test_acc.append(find_accuracy(test_set))
+
+
+
+print 'test accuracies: ' + str(test_acc)
+#print 'validation accuracies: ' + str(val_acc)
+plt.plot(max_depths, test_acc, 'bo')
+plt.xlabel('maximum depth')
+plt.ylabel('accuracies')
+plt.show()
