224baef6970fa98b314a86a471af1710ba3b6348
Tech Kuo <thk42@cornell.edu>
2014-09-23 01:02:19 -0400

    Finished code on Problem 3a, 3b

--- Homework 2/problem_3/CS4780HW2Problem3.py
+++ Homework 2/problem_3/CS4780HW2Problem3.py
@@ -5,7 +5,9 @@
 
 import numpy as np
 import sys
+import math
 
+# Part A Part i
 def get_prediction_arrays(f):
     data_file = open(f)
     pred_arrays = [[],[]]
@@ -24,16 +26,16 @@ def get_stats(pa):
     for i in range(2):
         means[i] = np.mean(pa[i])
         stds[i] = np.std(pa[i])
-    return [means,stds]
+    return [means,stds, len(pa[0])]
 
 def get_confidence(stats):
     cis = []
     z95 = 1.96
     for i in range(2):
-        lo = stats[i][0] - (z95 * stats[i][1])
+        lo = stats[i][0] - (z95 * stats[i][1]/float(math.sqrt(stats[2])))
         if lo < 0:
             lo = 0
-        hi = stats[i][0] + (z95 * stats[i][1])
+        hi = stats[i][0] + (z95 * stats[i][1]/float(math.sqrt(stats[2])))
         if hi > 1:
             hi = 1
         ci = (lo,hi)
@@ -46,23 +48,17 @@ cis = get_confidence(stats)
 print "\nConfidence interval for kNN: " + str(cis[0]) + "\n"
 print "Confidence interval for Decision Tree: " + str(cis[1]) + "\n" """
 
-def get_error_rates(f):
+# Part A Part ii
+def get_differences(f):
     data_file = open(f)
-    total_num = 0 # holds total num of observations
-    error_rates = [0,0]
+    differences = [0,0]      # holds [d1,d2]
     for line in data_file:
-        total_num += 1
         contents = line.split(',')
-        for i in range(1,3):
-            if i == 1:
-                if contents[0] != contents[i]:
-                    error_rates[i-1]+=1 # holds num_errors for kNN
-            if i == 2:
-                if contents[0] != (contents[i].strip()):
-                    error_rates[i-1]+=1 # holds num_erros for decision tree
-    for i in range(2):
-        error_rates[i] = error_rates[i]/float(total_num)
-    return error_rates
+        if (contents[0] != contents[1]) and (contents[0] == (contents[2].strip())): # h1 makes an error, h2 is correct
+            differences[0]+=1
+        if (contents[0] != (contents[2].strip())) and (contents[0] == contents[1]):  # h2 makes an error, h1 is correct
+            differences[1]+=1
+    return differences
 
-er = get_error_rates(sys.argv[1])
-print er
+dif = get_differences(sys.argv[1])
+print dif
---
+++ Homework 2/problem_3/CS4780HW2Problem3B.py
@@ -0,0 +1,72 @@
+import math
+import matplotlib.pyplot as plt
+
+train_x = [1,1,2,2,4,6,3,3,4,5,5,6]
+train_y = [4,6,1,2,1,1,3,4,3,5,6,5]
+train_color = ['red','red','red','red','red','red','blue','blue','blue','blue','blue','blue']
+
+training_set = []
+
+xblue = []
+yblue = []
+
+xred = []
+yred = []
+
+
+class Point:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def distance(self, x, y):
+        return math.sqrt((self.x-x) **2 + (self.y-y) **2)
+
+def find_nearest_neighbors(x, y, k):
+    def cmp(p1, p2):
+        diff = p1.distance(x, y) - p2.distance(x, y)
+        if diff < 0:
+            return -1
+        elif diff == 0:
+            return 0
+        else:
+            return 1
+    sorted_points = sorted(training_set, cmp)
+    return sorted_points[:k]
+
+def store_point(x, y, neighbors):
+    red = 0
+    blue = 0
+    for point in neighbors:
+        if point.color == "blue":
+            blue += 1
+        else:
+            red += 1
+    if red > blue:
+        xred.append(x)
+        yred.append(y)
+    else:
+        xblue.append(x)
+        yblue.append(y)
+
+
+for i in range(6):
+    new_point = Point(train_x[i], train_y[i], train_color[i])
+    training_set.append(new_point)
+x_range = range(-16,64)
+for i in range(len(x_range)):
+    x_range[i] /= 8.0
+y_range = range(-80,240,5)
+for i in range(len(y_range)):
+    y_range[i] /= 8.0
+
+for x in x_range:
+    for y in y_range:
+        neighbors = find_nearest_neighbors(x, y, 1)
+        store_point(x, y, neighbors)
+plt.plot(xred, yred, 'ro')
+plt.plot(xblue, yblue, 'bo')
+plt.ylabel('y')
+plt.xlabel('x')
+plt.show()
